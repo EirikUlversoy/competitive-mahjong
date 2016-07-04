0944839d18563e5e90e7dc2e91963816c855aaa0
Eirik <Eulverso@gmail.com>
2016-07-04 19:17:35 +0200

    Add submodule tiles

---
+++ competitive-mahjong-representation-tiles/src/main/java/Suit.java
@@ -0,0 +1,11 @@
+public class Suit {
+    String identifier;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+}
